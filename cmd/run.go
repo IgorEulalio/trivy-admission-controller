f80aafdf2e04ff206f41c03c725c5426f2ab8ad4
Igor Eulalio <igor.eulalio@sysdig.com>
2024-07-17 14:11:41 -0300

    add cert files in the initial log lines to help troubleshooting

--- cmd/run.go
+++ cmd/run.go
@@ -19,7 +19,7 @@ func Run() {
 
 	configuration.InitConfig()
 	config := configuration.Cfg
-
+	logger.Debug().Msg("Config successfully loaded.")
 	c, err := cache.NewCacheFromConfig(config)
 	if err != nil {
 		logger.Fatal().Msgf("Error creating cache: %v", err)
@@ -33,6 +33,6 @@ func Run() {
 	handler := api.NewHandler(c, kubernetes.GetClient())
 
 	http.HandleFunc("/validate", handler.Validate)
-	logger.Info().Msgf("Starting server on port %v", config.Port)
+	logger.Info().Msgf("Starting server on port %v, using certificate file %v and certificate key %v", config.Port, config.CertFile, config.KeyFile)
 	log.Fatal(http.ListenAndServeTLS(fmt.Sprintf("%s:%v", "0.0.0.0", config.Port), config.CertFile, config.KeyFile, nil))
 }
